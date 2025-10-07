af02763658fe8a75e5b285d16d26a538726aa2be
sumi-kml <sumeyakemal04@gmail.com>
2025-10-07 13:20:37 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -4,23 +4,25 @@ import math
 
 GPIO.setmode(GPIO.BCM)
 
-p = 17
+p1 = 17
+p2 = 27
 base_f = 500
 f = 0.2
+phi = math.pi / 11
 
 GPIO.setup(p, GPIO.OUT)
 
-pwm = GPIO.PWM(p, base_f) 
-pwm.start(0) 
+pwm1 = GPIO.PWM(p1, base_f) 
+pwm2 = GPIO.PWM(p2, base_f) 
+pwm1.start(0) 
+pwm2.start(0) 
 
 try:
 	while 1:
 		t = time.time()
-		b = (math.sin(2 * math.pi * f * t)) ** 2
-		dc = b * 100  
-		pwm.ChangeDutyCycle(dc)
+		b1 = (math.sin(2 * math.pi * f * t)) ** 2
+		b2 = (math.sin(2 * math.pi * f * t - phi)) ** 2 
+		pwm1.ChangeDutyCycle(100 * b1)
+		pwm2.ChangeDutyCycle(100 * b2)
 except:
 	GPIO.cleanup()
-
-
-
