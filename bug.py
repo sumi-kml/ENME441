fdde4fc0501f5039ce8116b6c697f02081bdd831
sumi-kml <sumeyakemal04@gmail.com>
2025-10-14 17:48:35 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -13,33 +13,26 @@ GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 class Bug:
-    def __init__(self, timestep = 0.1, x = 3, isWrapOn = False):
+class Bug:
+    def __init__(self, timestep=0.1, x=3, isWrapOn=False):
         self.timestep = timestep
         self.x = x
         self.isWrapOn = isWrapOn
         self.__shifter = Shifter(23, 25, 24)
-        self.run = False
 
     def start(self):
-        self.run = True
-        try:
-            while self.run: 
-                pattern = 1 << self.x
-                self.__shifter.shiftByte(pattern)
-                time.sleep(self.timestep)
-            
-                step = random.choice([-1, 1])
-                self.x += step
-    
-                if self.isWrapOn == True:
-                    self.x = (self.x) % 8              
-                else:
-                    self.x = max(0, min(7, self.x))
-        except KeyboardInterrupt:
-            self.stop()
+        pattern = 1 << self.x
+        self.__shifter.shiftByte(pattern)
+
+        step = random.choice([-1, 1])
+        self.x += step
+
+        if self.isWrapOn:
+            self.x = self.x % 8
+        else:
+            self.x = max(0, min(7, self.x))
 
     def stop(self):
-        self.run = False
         self.__shifter.shiftByte(0)
 
 b = Bug()
@@ -51,11 +44,8 @@ GPIO.add_event_detect(s2, GPIO.BOTH, callback=flip, bouncetime=500)
         
 try:
     while True:
-        print("s1:", GPIO.input(s1), "s3:", GPIO.input(s3))
         if GPIO.input(s1):
-            if not b.run:
-                b.run = True
-                threading.Thread(target=b.start).start()
+            b.start()
         else:
             b.stop()
 
@@ -63,10 +53,14 @@ try:
             b.timestep = 0.1 / 3
         else:
             b.timestep = 0.1
+
+        time.sleep(b.timestep)
 except KeyboardInterrupt:
     pass
 finally:
     GPIO.cleanup()
+
+
 
 
     
