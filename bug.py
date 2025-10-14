72af187e3192342b6673bd50d4a1e1d392b3db07
sumi-kml <sumeyakemal04@gmail.com>
2025-10-14 16:11:40 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -7,18 +7,11 @@ b = Shifter(23, 25, 24)
 x = 0 
 
 while True:
-    pattern = 1 << position
+    pattern = 1 << x
     b.shiftByte(pattern)
     time.sleep(0.05)
 
     step = random.choice([-1, 1])
-    position += step
-
-    position = max(0, min(7, position))
-
-try:
-  b = Shifter(23, 25, 24)
-  b.shiftByte(0b01100111)
-  while 1: pass
-except:
-  GPIO.cleanup()  
+    x += step
+    
+    x = max(0, min(7, x))
