27d447979557dda108bf4b8b2aa1f185eca3b6f4
sumi-kml <sumeyakemal04@gmail.com>
2025-10-14 15:21:22 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -6,22 +6,26 @@ GPIO.setmode(GPIO.BCM)
 dataPin, latchPin, clockPin = 23, 24, 25
 
 GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
-GPIO.setup(clockPin, GPIO.OUT, initial=0)  
+GPIO.setup(latchPin, GPIO.OUT, initial=0) 
+GPIO.setup(clockPin, GPIO.OUT, initial=0)
 
-pattern = 0b01100100        # 8-bit pattern to display on LED bar
+pattern = 0b01100110 
 
-for i in range(8):
-  GPIO.output(dataPin, pattern & (1<<i))
-  GPIO.output(clockPin,1) 	    # ping the clock pin to shift register data
+def ping(p): 
+  GPIO.output(p,1)
   time.sleep(0)
-  GPIO.output(clockPin,0)
-
-GPIO.output(latchPin, 1)        # ping the latch pin to send register to output
-time.sleep(0)
-GPIO.output(latchPin, 0)
-
+  GPIO.output(p,0)
+  
+def shiftByte(b):
+  for i in range(8):
+    GPIO.output(dataPin, b & (1<<i))
+    ping(clockPin) 
+  ping(latchPin) 
+  
 try:
-  while 1: pass
+  while 1:
+    for i in range(2**8):
+      shiftByte(i)
+      time.sleep(0.5)
 except:
   GPIO.cleanup()
