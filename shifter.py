2479e7e28b6942db323422796ab5b9f9dd8dc5b6
sumi-kml <sumeyakemal04@gmail.com>
2025-10-14 15:26:54 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -6,26 +6,24 @@ GPIO.setmode(GPIO.BCM)
 dataPin, latchPin, clockPin = 23, 24, 25
 
 GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0) 
+GPIO.setup(latchPin, GPIO.OUT, initial=0)
 GPIO.setup(clockPin, GPIO.OUT, initial=0)
 
 pattern = 0b01100110 
 
-def ping(p): 
+def ping(p):
   GPIO.output(p,1)
   time.sleep(0)
   GPIO.output(p,0)
-  
-def shiftByte(b):
+
+def shiftByte(b): 
   for i in range(8):
     GPIO.output(dataPin, b & (1<<i))
     ping(clockPin) 
   ping(latchPin) 
-  
+
 try:
-  while 1:
-    for i in range(2**8):
-      shiftByte(i)
-      time.sleep(0.5)
+  shiftByte(0b01100110) 
+  while 1: pass
 except:
-  GPIO.cleanup()
+  GPIO.cleanup()  
