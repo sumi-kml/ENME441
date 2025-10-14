dd38923a6043b1594c9c35c4846bad563677e6b6
sumi-kml <sumeyakemal04@gmail.com>
2025-10-14 15:43:39 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -1,29 +1,34 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+class Shifter:
+  def __init__(self, serialPin, clockPin, latchPin):
+    self.serialPin = serialPin
+    self.latchPin = latchPin
+    self.clockPin = clockPin
+    
+    GPIO.setmode(GPIO.BCM)
 
-dataPin, latchPin, clockPin = 23, 24, 25
+    GPIO.setup(self.serialPin, GPIO.OUT)
+    GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
+    GPIO.setup(self.clockPin, GPIO.OUT, initial=0)
 
-GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0)
-GPIO.setup(clockPin, GPIO.OUT, initial=0)
+    pattern = 0b01100110 
+    
+  def __ping(self, p):
+    GPIO.output(p,1)
+    time.sleep(0)
+    GPIO.output(p,0)
 
-pattern = 0b01100110 
-
-def ping(p):
-  GPIO.output(p,1)
-  time.sleep(0)
-  GPIO.output(p,0)
-
-def shiftByte(b): 
-  for i in range(8):
-    GPIO.output(dataPin, b & (1<<i))
-    ping(clockPin) 
-  ping(latchPin) 
+  def shiftByte(self, b): 
+    for i in range(8):
+      GPIO.output(self.serialPin, b & (1<<i))
+      self.__ping(self.clockPin) 
+    self.__ping(self.latchPin) 
 
 try:
-  shiftByte(0b01100110) 
+  a = Shifter(23, 25, 24)
+  a.shiftByte(0b01100110)
   while 1: pass
 except:
   GPIO.cleanup()  
