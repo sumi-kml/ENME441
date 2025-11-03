f1b92b6b4ab8bbd06679044810741914bf85ac22
sumi-kml <sumeyakemal04@gmail.com>
2025-11-03 16:22:24 -0500

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -101,11 +101,12 @@ if __name__ == '__main__':
 
     # Use multiprocessing.Lock() to prevent motors from trying to 
     # execute multiple operations at the same time:
-    lock = multiprocessing.Lock()
+    lock1 = multiprocessing.Lock()
+    lock2 = multiprocessing.Lock()
 
     # Instantiate 2 Steppers:
-    m1 = Stepper(s, lock)
-    m2 = Stepper(s, lock)
+    m1 = Stepper(s, lock1)
+    m2 = Stepper(s, lock2)
 
     # Zero the motors:
     m1.zero()
